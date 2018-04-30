366cc2a2f13356063f0f22e9c256505d080b09c5
hevlitt <holliver.v@gmail.com>
2018-04-30 02:39:30 -0600

    Initial commit

---
+++ app/src/main/java/com/example/win10/a4_3_graficos2d/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.win10.a4_3_graficos2d;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
